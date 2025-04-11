e71dc2734ad5e17cdb0b318015f8e6c984ceddb9
Shailesh Gokhale <68338746+shailesh-ss-19-11@users.noreply.github.com>
2025-04-11 10:41:23 +0530

    loader

--- src/main.jsx
+++ src/main.jsx
@@ -6,37 +6,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { routes } from './routes/Routes.js'
 import Protected from './protectAuth/Protected.jsx'
 import Login from './auth/Login.jsx'
-import { ToastContainer } from 'react-toastify';
+import Loader from './components/Loader.jsx';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/login' Component={Login} />
-          {/* <Route path='/' element={<App />} /> */}
-          {/* <Route path='/' Component={App} /> */}
-          {/* <Route path='/services/' Component={Services} >
-          <Route path='home-service' Component={HomeServices}/>
-        </Route> */}
-          {
-            routes &&
-            routes.map((route) => {
-              return (
-                // parent routes ///////////////////////////////////
-                <Route path={route.path} Component={route.component}>
-                  {/* //nested routes //////////////////// */}
-                  {route && route.children &&
-                    route.children.map((child) => {
-                      return <Route path={child.path} Component={child.component} />
-                    })}
-                </Route>
-              )
-            })
-          }
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-    <ToastContainer />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Navbar />
+			<Suspense fallback={<Loader/>}>
+				<Routes>
+					<Route path='/login' Component={Login} />
+					{/* <Route path='/' element={<App />} /> */}
+					{/* <Route path='/' Component={App} /> */}
+					{/* <Route path='/services/' Component={Services} >
+					<Route path='home-service' Component={HomeServices}/>
+				</Route> */}
+					{
+						routes &&
+						routes.map((route) => {
+							return (
+								// parent routes ///////////////////////////////////
+								<Route path={route.path} element={<Protected>
+									<route.component/>
+								</Protected>}>
+									{/* //nested routes //////////////////// */}
+									{route && route.children &&
+										route.children.map((child) => {
+											return <Route path={child.path} Component={child.component} />
+										})}
+								</Route>
+							)
+						})
+					}
+				</Routes>
+			</Suspense>
+		</BrowserRouter>
+	</StrictMode>,
 )
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,70 +1,68 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
-const App = lazy(()=>import('./../App'))
-// import App from "../App";
-import Login from "../auth/Login";
-import AddCustomer from "../customer/AddCustomer";
-import Customer from "../customer/Customer";
-import CustomerDetails from "../customer/CustomerDetails";
-import UpdateCustomer from "../customer/UpdateCustomer";
-import Page404NotFound from "../Page404NotFound";
-import About from "../web-content/About";
-import Contact from "../web-content/Contact";
-import HomeServices from "../web-content/HomeServices";
-import Outsourcing from "../web-content/Outsoursing";
-import Services from "../web-content/Services";
-import Testimonial from "../web-content/Testimonial";
+const App = lazy(() => import("./../App"));
+const AddCustomer = lazy(() => import("../customer/AddCustomer"));
+const Customer = lazy(() => import("../customer/Customer"));
+const CustomerDetails = lazy(() => import("../customer/CustomerDetails"));
+const UpdateCustomer = lazy(() => import("../customer/UpdateCustomer"));
+const About = lazy(() => import("../web-content/About"));
+const Contact = lazy(() => import("../web-content/Contact"));
+const HomeServices = lazy(() => import("../web-content/HomeServices"));
+const Outsourcing = lazy(() => import("../web-content/Outsoursing"));
+const Services = lazy(() => import("../web-content/Services"));
+const Testimonial = lazy(() => import("../web-content/Testimonial"));
+const Page404NotFound = lazy(() => import("../Page404NotFound"));
+
 export const routes = [
-    {
-        path: '*',
-        component: Page404NotFound
-    },
-    {
-        path: '/home',
-        component: App
-    },
-    {
-        path: '/about',
-        component: About
-    },
-    {
-        path: '/services',
-        component: Services,
-        // nested routes ///////////////////////////////////////
-        children:[
-            {
-                path:"/services/home-service",
-                component: HomeServices
-            },
-            {
-                path:"/services/outsourcing",
-                component: Outsourcing
-            }
-        ]
-    },
-    {
-        path: '/contact',
-        component: Contact
-    },
-    {
-        path: "/testimonial",
-        component: Testimonial
-    },
-    {
-        path: "/customer",
-        component: Customer
-    },
-    {
-        path: "customer/add",
-        component: AddCustomer
-    },
-    {
-        path: '/customer/details/:id',
-        component: CustomerDetails
-    },
-    {
-        path: '/customer/update/:id',
-        component: UpdateCustomer
-    }
-
-]
+  {
+    path: "*",
+    component: Page404NotFound,
+  },
+  {
+    path: "/home",
+    component: App,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/services",
+    component: Services,
+    // nested routes ///////////////////////////////////////
+    children: [
+      {
+        path: "/services/home-service",
+        component: HomeServices,
+      },
+      {
+        path: "/services/outsourcing",
+        component: Outsourcing,
+      },
+    ],
+  },
+  {
+    path: "/contact",
+    component: Contact,
+  },
+  {
+    path: "/testimonial",
+    component: Testimonial,
+  },
+  {
+    path: "/customer",
+    component: Customer,
+  },
+  {
+    path: "customer/add",
+    component: AddCustomer,
+  },
+  {
+    path: "/customer/details/:id",
+    component: CustomerDetails,
+  },
+  {
+    path: "/customer/update/:id",
+    component: UpdateCustomer,
+  },
+];
