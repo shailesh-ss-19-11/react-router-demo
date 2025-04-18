8a3c7ab8d18bba3b98678d6915bcd12ad8a2076e
Shailesh Gokhale <68338746+shailesh-ss-19-11@users.noreply.github.com>
2025-04-18 09:36:20 +0530

    setstate new method

--- src/usecallbackexp/Counter.jsx
+++ src/usecallbackexp/Counter.jsx
@@ -4,13 +4,21 @@ import Todos from "./Todos";
 const Counter = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
-
+  const [obj, setobj] = useState({ name: "shailesh", age: 23 });
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
+    setCount((prev) => Number(prev) + 1);
+    // let newobj = {...obj,address:"nagpur"}
+    // setobj(newobj);
+
+    setobj((prev) => {
+      return { ...prev, address: "nagpur" };
+    });
   };
+  console.log(obj, "obj");
 
   const addTodo = useCallback(() => {
-    setTodos((t) => [...t, "New Todo"]);
+    setTodos((prev) => [...prev, "New Todo"]);
   }, [todos]);
 
   //   const addTodo = () => {
